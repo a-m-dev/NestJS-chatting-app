8158a53daf82a4f57e4aad0916bb2bb7612ae8db
a-m-dev <a.mirzaei.dev@gmail.com>
2024-09-30 14:43:12 +0200

    feat: graphql set up + users module

--- chatter-backend/src/common/database/database.module.ts
+++ chatter-backend/src/common/database/database.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRootAsync({
+      // useFactory injects services in nest js
       useFactory: (configService: ConfigService) => ({
         uri: configService.get('MONGODB_URI'),
       }),
